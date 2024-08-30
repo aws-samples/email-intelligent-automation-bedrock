ff5ab08c2289efb83d8a9c1ba789aba5a1d2ba28
Godwin Vincent <87547275+vgodwinamz@users.noreply.github.com>
2024-08-30 00:08:46 -0600

    Update fm_onprem_inference.py

--- network_logs/fm_onprem/fm_onprem_inference.py
+++ network_logs/fm_onprem/fm_onprem_inference.py
@@ -3,61 +3,72 @@ from transformers import AutoTokenizer
 import logging, sys
 import warnings
 import boto3
-import argparse
+#import argparse
+import streamlit as st
 
 bedrock_agent_runtime_client = boto3.client("bedrock-agent-runtime", region_name='us-west-2')
 
-parser = argparse.ArgumentParser(description='Aggument parser to call Bedrock Models')
-parser.add_argument('--kbid', type=str, required=True, help='Knowledgebase ID')
-parser.add_argument('--prompt', type=str, required=True, help='user prompt')
-args = parser.parse_args()
-
-query = args.prompt
-kb_id = args.kbid
-
-'''messages = [
-    {"role": "system", "content": "You are a network admin and helps to trouble shoot issue based on the user imput"},
-    {"role": "user", "content": query},
-]'''
 # retrieve api for fetching only the relevant context.
-relevant_documents = bedrock_agent_runtime_client.retrieve(
-    retrievalQuery= {
-        'text': query
-    },
-    knowledgeBaseId=kb_id,
-    retrievalConfiguration= {
-        'vectorSearchConfiguration': {
-            'numberOfResults': 3 # will fetch top 3 documents which matches closely with the query.
-        }
-    }
-)
+def main():
+    st.title("Network Admin Chatbot")
+    st.subheader("Sample Questions")
+    sample_questions = [
+        "Do you see any unusual traffic in this week?",
+        "Any suspicious IP connection?",
+        "Can you detect any unusual traffic patterns?",
+        "Were there any security events reported?",
+        "Are there any performance issues?",
+        "Are there any configuration problems?",
+        "What is considered normal activity?"
+    ]
+    for question in sample_questions:
+        st.markdown(f"- {question}")
 
-rel_docs=relevant_documents["retrievalResults"]
-rel_docs1=""
-for i in rel_docs:
-        rel_docs1 = rel_docs1+(i['content']['text'])+"/n"
+    query = st.text_input("Enter your query:")
+    
+    if query:
+        relevant_documents = bedrock_agent_runtime_client.retrieve(
+            retrievalQuery= {
+                'text': query
+            },
+            knowledgeBaseId='8YJ7JS8IGW',
+            retrievalConfiguration= {
+                'vectorSearchConfiguration': {
+                    'numberOfResults': 3 # will fetch top 3 documents which matches closely with the query.
+                }
+            }
+        )
 
-messages = [
-    {"role": "system", "content": "You are a network admin and helps to trouble shoot issue based on the user imput"},
-    {"role": "user", "content": rel_docs1},
-]
+        rel_docs=relevant_documents["retrievalResults"]
+        rel_docs1=""
+        for i in rel_docs:
+                rel_docs1 = rel_docs1+(i['content']['text'])+"/n"
 
-logging.disable(sys.maxsize)
-warnings.filterwarnings("ignore")
-model_id = "neuralmagic/Meta-Llama-3.1-8B-Instruct-quantized.w4a16"
+        messages = [
+            {"role": "system", "content": "You are a network admin and helps to trouble shoot issue based on the user imput"},
+            {"role": "user", "content": rel_docs1},
+        ]
 
-number_gpus = 1
-max_model_len = 8192
+        logging.disable(sys.maxsize)
+        warnings.filterwarnings("ignore")
+        model_id = "neuralmagic/Meta-Llama-3.1-8B-Instruct-quantized.w4a16"
 
-sampling_params = SamplingParams(temperature=0.6, top_p=0.9, max_tokens=256)
+        number_gpus = 1
+        max_model_len = 8192
 
-tokenizer = AutoTokenizer.from_pretrained(model_id)
+        sampling_params = SamplingParams(temperature=0.1, top_p=0.9, max_tokens=1024)
 
-prompts = tokenizer.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-llm = LLM(model=model_id, tensor_parallel_size=number_gpus, max_model_len=max_model_len)
+        prompts = tokenizer.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
 
-outputs = llm.generate(prompts, sampling_params)
+        llm = LLM(model=model_id, tensor_parallel_size=number_gpus, max_model_len=max_model_len)
 
-generated_text = outputs[0].outputs[0].text
-print(generated_text)
+        outputs = llm.generate(prompts, sampling_params)
+
+        generated_text = outputs[0].outputs[0].text
+
+        st.text_area("Response:", value=generated_text,height = 400)
+
+if __name__ == "__main__":
+    main()
